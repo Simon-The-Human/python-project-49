b407c02e36a0c4393433e84bf57304f348d17ab9
Семен Серов <sserov@astralinux.ru>
2023-09-28 14:37:49 +0300

    rename

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -9,23 +9,23 @@ def welcome_user():
     print(f"Hello, {username}!")
 
 
-def hcf(num1, num2):
+def gcd(num1, num2):
     while num2 != 0:
         num1, num2 = num2, num1 % num2
     return num1
 
-def hcf_game():
+def gcd_game():
     print('What is the result of the expression?')
     for _ in range(3):
         num1 = random.randrange(1, 100)
         num2 = random.randrange(1, 100)
-        hcf_num = hcf(num1, num2)
+        gcd_num = gcd(num1, num2)
         print(f'Question: {num1} {num2}')
         ans = int(input(f'Your answer: '))
-        if ans == hcf_num:
+        if ans == gcd_num:
             print('Correct!')
         else:
-            print(f"'{ans}' is wrong answer ;(. Correct answer was '{hcf_num}'.\nLet's try again, {username}!")
+            print(f"'{ans}' is wrong answer ;(. Correct answer was '{gcd_num}'.\nLet's try again, {username}!")
             break
     else:
         print(f'Congratulations, {username}!')
@@ -34,7 +34,7 @@ def hcf_game():
 def main():
     print("Welcome to the Brain Games!")
     welcome_user()
-    hcf_game()
+    gcd_game()
 
 
 if __name__ == "__main__":
