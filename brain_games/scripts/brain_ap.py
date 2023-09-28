e01947d1ded5b5f59c95468e0a88969d1c9a9612
Семен Серов <sserov@astralinux.ru>
2023-09-28 13:18:24 +0300

    added arithmetic progression game

---
+++ brain_games/scripts/brain_ap.py
@@ -0,0 +1,57 @@
+from .. import cli
+import random
+import prompt
+
+
+def welcome_user():
+    global username
+    username = prompt.string("Present yourself: ")
+    print(f"Hello, {username}!")
+
+
+def ap(num, coef):
+    raw = list()
+    c = num
+    for _ in range(10):
+        raw.append(c)
+        c += coef
+    return raw
+
+
+def ap_q(raw):
+    c = random.randint(0, 9)
+    raw_q = {
+        "raw": raw,
+        "c": c,
+        "ans": raw[c]
+    }
+    raw_q["raw"][c] = '..'
+    return raw_q
+
+
+def calc_game():
+    print('What number is missing in the progression?')
+    for _ in range(3):
+        num = random.randrange(1, 100)
+        coef = random.randrange(1, 20)
+        raw = ap(num, coef)
+        raw_q = ap_q(raw)
+        print(f'Question: {raw_q["raw"]}')
+        ans = int(input(f'Your answer: '))
+        if ans == raw_q["ans"]:
+            print('Correct!')
+        else:
+            print(f"'{ans}' is wrong answer ;(. Correct answer was '{raw_q['ans']}'.\nLet's try again, {username}!")
+            break
+    else:
+        print(f'Congratulations, {username}!')
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    welcome_user()
+    calc_game()
+
+
+if __name__ == "__main__":
+    main()
