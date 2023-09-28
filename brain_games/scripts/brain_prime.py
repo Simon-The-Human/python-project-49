474076ca17afce4e058e1efb6bee877cf09e59dd
Семен Серов <sserov@astralinux.ru>
2023-09-28 13:54:06 +0300

    added script brain game prime digit

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from .. import cli
+import random
+import prompt
+
+
+def welcome_user():
+    global username
+    username = prompt.string("Present yourself: ")
+    print(f"Hello, {username}!")
+
+
+def prime(num):
+    prime = 'yes'
+    for i in range(2, num // 2 + 1):
+        if num % i == 0:
+            prime = 'no'
+    return prime
+
+
+def prime_game():    
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    for _ in range(3):
+        num = random.randrange(1, 100)
+        print(f'Question: {num}')
+        is_prime = prime(num)
+        ans = input(f'Your answer: ')
+        if ans == is_prime:
+            print('Correct!')
+        else:
+            print(f"'{ans}' is wrong answer ;(. Correct answer was '{is_prime}'.\nLet's try again, {username}!")
+            break
+    else:
+        print(f'Congratulations, {username}!')
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    welcome_user()
+    prime_game()
+
+
+if __name__ == "__main__":
+    main()
